aabbdd6c6e1af078882b18dea3b4569cf65d177e
Ben-Sicat <bensicat00@gmail.com>
2022-08-02 07:43:25 +0800

    day2

--- DSA/array.py
+++ DSA/array.py
@@ -28,3 +28,25 @@ def twoSum(self, nums: list[int], target: int) -> list[int]:
         if diff in hash:
             return [hash[diff], i]#since were looking for the index of the number in the list, we need to return the index of the number in the list
         hash[nums[i]] = i
+#88 Easy
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> list[int]:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    
+    last = n + m -1
+
+    while m > 0 and n > 0:
+        if nums1[m-1] > nums2[n-1]:
+            nums1[last] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[last] = nums2[n-1]
+            print("heh")
+            n -= 1
+        last -= 1
+    while n > 0:
+        nums1[last] = nums2[n-1]
+        n, last  = n-1, last-1
+    return nums1
+print(merge([0], 0, [1], 1))
