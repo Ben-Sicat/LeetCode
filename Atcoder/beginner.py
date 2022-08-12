26a4a8ae17835c0b8b516d6b06cb57c2ce836678
Ben-Sicat <bensicat00@gmail.com>
2022-08-12 19:07:15 +0800

    cheating

--- Atcoder/beginner.py
+++ Atcoder/beginner.py
@@ -11,9 +11,10 @@ print(worldCup(0, 3000))
 def traingle(self, numRow: int) -> list[list[int]]:
     res = [[1]]
 
-    for i in range(1, numRow):
-        res.append([1] * (i + 1))
-        for j in range(1, i):
-            res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+    for i in range(numRow -1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(1, len(temp) -1):
+            row.append(temp[j-1] + temp[j])
+        res.append(row)
     return res
-    
